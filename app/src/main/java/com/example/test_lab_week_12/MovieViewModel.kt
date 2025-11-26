b3089eb62a649441cc91b24f04af980172516ca4
Jerikhorr <133670400+Jerikhorr@users.noreply.github.com>
2025-11-26 18:34:37 +0700

    Commit No. 4 – Assignment

--- app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -12,8 +12,6 @@ import kotlinx.coroutines.launch
 class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
 
     // define the StateFlow in replace of the LiveData
-    // a StateFlow is an observable Flow that emits state updates to the collectors
-    // MutableStateFlow is a StateFlow that you can change the value
     private val _popularMovies = MutableStateFlow<List<Movie>>(emptyList())
     val popularMovies: StateFlow<List<Movie>> = _popularMovies
 
@@ -26,18 +24,16 @@ class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel()
 
     // fetch movies from the API
     private fun fetchPopularMovies() {
-        // launch a coroutine in viewModelScope
-        // Dispatchers.IO means that this coroutine will run on a shared pool of threads
         viewModelScope.launch(Dispatchers.IO) {
             movieRepository.fetchMovies()
                 .catch { exception ->
-                    // catch is a terminal operator that catches exceptions from the Flow
                     _error.value = "An exception occurred: ${exception.message}"
                 }
                 .collect { movies ->
-                    // collect is a terminal operator that collects the values from the Flow
-                    // the results are emitted to the StateFlow
-                    _popularMovies.value = movies
+                    // ASSIGNMENT: Implement data filter (descending by popularity)
+                    // Mengurutkan movie berdasarkan popularitas tertinggi ke terendah
+                    // sebelum di-emit ke StateFlow
+                    _popularMovies.value = movies.sortedByDescending { it.popularity }
                 }
         }
     }
