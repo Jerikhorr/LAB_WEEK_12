143a3ab6c0f062561b58a67efc0f440a09409e0e
Jerikhorr <133670400+Jerikhorr@users.noreply.github.com>
2025-11-26 18:24:44 +0700

    Commit No. 2 – Building app with coroutines

--- app/src/main/java/com/example/test_lab_week_12/MainActivity.kt
+++ app/src/main/java/com/example/test_lab_week_12/MainActivity.kt
@@ -3,27 +3,62 @@ package com.example.test_lab_week_12
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.example.test_lab_week_12.model.Movie
+import com.google.android.material.snackbar.Snackbar
+import java.util.Calendar
 
-class MainActivity : AppCompatActivity() {
-    private val movieAdapter by lazy {
-        MovieAdapter(object : MovieAdapter.MovieClickListener {
-            override fun onMovieClick(movie: Movie) {
-                openMovieDetails(movie)
-            }
-        })
-    }
+class MainActivity : AppCompatActivity(), MovieAdapter.MovieClickListener {
+
+    private lateinit var movieAdapter: MovieAdapter
+    private lateinit var movieViewModel: MovieViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Setup adapter
+        movieAdapter = MovieAdapter(this)
+
         val recyclerView: RecyclerView = findViewById(R.id.movie_list)
         recyclerView.adapter = movieAdapter
+
+        // Ambil repository dari MovieApplication
+        val movieRepository = (application as MovieApplication).movieRepository
+
+        // Setup ViewModel
+        movieViewModel = ViewModelProvider(
+            this,
+            object : ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return MovieViewModel(movieRepository) as T
+                }
+            }
+        )[MovieViewModel::class.java]
+
+        // Observe movie list
+        movieViewModel.popularMovies.observe(this) { movieList ->
+            val year = Calendar.getInstance().get(Calendar.YEAR).toString()
+
+            movieAdapter.addMovies(
+                movieList
+                    //.filter { it.releaseDate?.startsWith(year) == true }
+                    .sortedByDescending { it.popularity }
+            )
+        }
+
+        // Observe error
+        movieViewModel.error.observe(this) { errorMsg ->
+            if (!errorMsg.isNullOrEmpty()) {
+                Snackbar.make(recyclerView, errorMsg, Snackbar.LENGTH_LONG).show()
+            }
+        }
     }
 
-    private fun openMovieDetails(movie: Movie) {
+    // Ketika item movie di-klik
+    override fun onMovieClick(movie: Movie) {
         val intent = Intent(this, DetailsActivity::class.java).apply {
             putExtra(DetailsActivity.EXTRA_TITLE, movie.title)
             putExtra(DetailsActivity.EXTRA_RELEASE, movie.releaseDate)
--- app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -1,7 +1,25 @@
 package com.example.test_lab_week_12
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-
-class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
-
+import androidx.lifecycle.viewModelScope
+import com.example.test_lab_week_12.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+class MovieViewModel(private val movieRepository: MovieRepository)
+    : ViewModel() {
+    init {
+        fetchPopularMovies()
+    }
+    // define the LiveData
+    val popularMovies: LiveData<List<Movie>>
+        get() = movieRepository.movies
+    val error: LiveData<String>
+        get() = movieRepository.error
+    // fetch movies from the API
+    private fun fetchPopularMovies() {
+        viewModelScope.launch(Dispatchers.IO) {
+            movieRepository.fetchMovies()
+        }
+    }
 }
