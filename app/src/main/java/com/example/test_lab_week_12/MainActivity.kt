e9980977674bd86b2798601d3e9fe84c7d393d9f
Jerikhorr <133670400+Jerikhorr@users.noreply.github.com>
2025-11-26 18:29:36 +0700

    Commit No. 3 – Upgrading app with flow

--- app/src/main/java/com/example/test_lab_week_12/MainActivity.kt
+++ app/src/main/java/com/example/test_lab_week_12/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.test_lab_week_12
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.RecyclerView
 import com.example.test_lab_week_12.model.Movie
 import com.google.android.material.snackbar.Snackbar
-import java.util.Calendar
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity(), MovieAdapter.MovieClickListener {
 
@@ -38,21 +41,31 @@ class MainActivity : AppCompatActivity(), MovieAdapter.MovieClickListener {
             }
         )[MovieViewModel::class.java]
 
-        // Observe movie list
-        movieViewModel.popularMovies.observe(this) { movieList ->
-            val year = Calendar.getInstance().get(Calendar.YEAR).toString()
-
-            movieAdapter.addMovies(
-                movieList
-                    //.filter { it.releaseDate?.startsWith(year) == true }
-                    .sortedByDescending { it.popularity }
-            )
-        }
-
-        // Observe error
-        movieViewModel.error.observe(this) { errorMsg ->
-            if (!errorMsg.isNullOrEmpty()) {
-                Snackbar.make(recyclerView, errorMsg, Snackbar.LENGTH_LONG).show()
+        // fetch movies from the API
+        // lifecycleScope is a lifecycle-aware coroutine scope
+        lifecycleScope.launch {
+            // repeatOnLifecycle is a lifecycle-aware coroutine builder
+            // Lifecycle.State.STARTED means that the coroutine will run
+            // when the activity is started
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    // collect the list of movies from the StateFlow
+                    movieViewModel.popularMovies.collect { movies ->
+                        // add the list of movies to the adapter
+                        movieAdapter.addMovies(movies)
+                    }
+                }
+                launch {
+                    // collect the error message from the StateFlow
+                    movieViewModel.error.collect { error ->
+                        // if an error occurs, show a Snackbar with the error message
+                        if (error.isNotEmpty()) {
+                            Snackbar.make(
+                                recyclerView, error, Snackbar.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+                }
             }
         }
     }
--- app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
@@ -1,29 +1,23 @@
 package com.example.test_lab_week_12
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.example.test_lab_week_12.api.MovieService
 import com.example.test_lab_week_12.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 class MovieRepository(private val movieService: MovieService) {
     private val apiKey = "284492c3e93ef3f8627225340fade7f8"
-    private val movieLiveData = MutableLiveData<List<Movie>>()
-    val movies: LiveData<List<Movie>>
-        get() = movieLiveData
-    private val errorLiveData = MutableLiveData<String>()
-    val error: LiveData<String>
-        get() = errorLiveData
 
-    suspend fun fetchMovies() {
-        try {
-            val popularMovies = movieService.getPopularMovies(apiKey)
-            movieLiveData.postValue(popularMovies.results)
-        } catch (exception: Exception) {
-            errorLiveData
-            errorLiveData.postValue(
-                "An error occurred: ${exception.message}"
-            )
-        }
+    // fetch movies from the API
+    // this function returns a Flow of Movie objects
+    // a Flow is a type of coroutine that can emit multiple values
+    fun fetchMovies(): Flow<List<Movie>> {
+        return flow {
+            // emit the list of popular movies from the API
+            emit(movieService.getPopularMovies(apiKey).results)
+            // use Dispatchers.IO to run this coroutine on a shared pool of threads
+        }.flowOn(Dispatchers.IO)
     }
 }
-
